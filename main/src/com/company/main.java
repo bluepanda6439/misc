fcbdc66dae7201782e5727943944b5a64910604d
Kasia <kathyy93@gmail.com>
2021-03-14 15:51:27 +0100

    added betterJustification

--- main/src/com/company/main.java
+++ main/src/com/company/main.java
@@ -5,9 +5,13 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class MyCode {
     public static void main (String[] args) {
-        assertEquals(justify("tekst", '=', 13),("====tekst===="));
-        assertEquals(justify("tekst", '=', 12),("====tekst==="));
-        assertTrue(justify("tekst", '=', 12).equals("====tekst==="));
+//        assertEquals(justify("tekst", '=', 13),("====tekst===="));
+//        assertEquals(justify("tekst", '=', 12),("====tekst==="));
+//        assertTrue(justify("tekst", '=', 12).equals("====tekst==="));
+
+        betterJustify("tekst", '=', 11);
+        betterJustify("something", '-', 17);
+        betterJustify("1dupa1", '+', 22);
     }
 
 
@@ -24,7 +28,8 @@ class MyCode {
             for (int i=0; i < temp; i++)
                 finalWord += justifyChar;
 
-            finalWord = finalWord+word+finalWord;}
+            finalWord = finalWord+word+finalWord;
+        }
         else{
             for (int i=0; i < (lengthWithoutWord+1)/2; i++){
                 finalWord += justifyChar;
@@ -38,6 +43,28 @@ class MyCode {
         }
 
 
+        System.out.println(finalWord);
+        return finalWord;
+    }
+
+    public static String betterJustify(String word, char justifyChar, int lengthAfterJustification){
+        int lengthWithoutWord = lengthAfterJustification - word.length();
+        String temp = "";
+        String temp2 = "";
+        String finalWord = "";
+
+        if (lengthWithoutWord % 2 == 0) {
+            for (int i = 0; i < lengthWithoutWord / 2; i++) {
+                temp += justifyChar;
+            }
+            finalWord = temp + word + temp;
+        }
+        else {
+            for (int i = 0; i < lengthWithoutWord-1/ 2; i++) {
+                temp2 += justifyChar;
+            }
+            finalWord = temp + word + temp2;
+        }
         System.out.println(finalWord);
         return finalWord;
     }
